55455d00e9dda75f27d829a3460f2d82deeff4d1
Bruno Casotto <casottoalves@hotmail.com>
2019-12-13 16:38:25 -0300

    Remove html entry

--- webpack/partials/entry.js
+++ webpack/partials/entry.js
@@ -1,12 +1,7 @@
 const path = require('path')
 const { rootPath } = require('./../utils/paths')
-const { isDev } = require('./../utils/enviroment')
 
 const entry = []
 entry.push(path.join(rootPath, 'index.js'))
-
-if(isDev) {
-  entry.push(path.join(rootPath, 'index.html'))
-}
 
 module.exports = entry
