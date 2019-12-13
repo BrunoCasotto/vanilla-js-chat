d5b693b7382b0653fea8cb1b0b1cef26ea1c7adf
Bruno Casotto <casottoalves@hotmail.com>
2019-12-13 16:51:13 -0300

    Html hot reload done

--- webpack/partials/dev-server.js
+++ webpack/partials/dev-server.js
@@ -6,6 +6,12 @@ const devServer =  {
   contentBase: path.join(rootPath, 'dist'),
   compress: true,
   port: 8080,
+  historyApiFallback: {
+    disableDotRule: true,
+    rewrites: [
+      { from: /^\/$/, to: path.join(rootPath, 'index.html') }
+    ]
+  }
 }
 
 module.exports = isDev ? devServer : {}
--- webpack/partials/plugins.js
+++ webpack/partials/plugins.js
@@ -1,3 +1,11 @@
-const plugins = []
+const path = require('path')
+const { rootPath } = require('./../utils/paths')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: path.join(rootPath, 'index.html')
+  })
+]
 
 module.exports = plugins
