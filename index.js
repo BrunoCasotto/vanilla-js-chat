e08510f48c7ee94f41e037eb31ab94ccc24e206c
Bruno Casotto <casottoalves@hotmail.com>
2019-12-14 10:48:00 -0300

    Example initialize script

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 import DomHandler from './src/dom-handler'
 import { chatVdom } from './src/vdoms'
 
-const chatTarget = document.querySelector('#vanilla-js-chat')
-DomHandler.render(chatTarget, chatVdom)
+/**
+ * Main method to initialize render and chat listeners
+ */
+const init = () => {
+  const chatTarget = document.querySelector('#vanilla-js-chat')
+  DomHandler.render(chatTarget, chatVdom)
+  // code
+}
+
+const restart = () => {
+  // code
+}
+
+const sendMessage = () => {
+  // code
+}
+
+const onSend = () => {
+  // code
+}
+
+const onClose = () => {
+  // code
+}
+
+const VanillaJsChat = {
+  init,
+  restart,
+  sendMessage,
+  onSend,
+  onClose,
+}
+
+window.VanillaJsChat = VanillaJsChat
