7b2db5c703b2a476b00404e154e054097f88cda4
Bruno Casotto <casottoalves@hotmail.com>
2019-12-14 11:00:03 -0300

    Comment on dom core

--- src/render/dom-handler.js
+++ src/render/dom-handler.js
@@ -1,5 +1,10 @@
 import { createElement } from './dom-core'
 
+/**
+ * Method to resolve vdom and render components
+ * @param {HTMLElement} wrapper when created element should be append
+ * @param {object} vdom object to represent dom
+ */
 const render = (wrapper, vdom) => {
   const createdElement = createElement(vdom)
   const { children } = vdom
