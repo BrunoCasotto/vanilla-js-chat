1b7c17679a70e4f14e591e3c2b9a0597a98d55dc
Bruno Casotto <casottoalves@hotmail.com>
2019-12-14 13:09:20 -0300

    Scope vanillaJsChat object

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
 import DomHandler from './src/render/dom-handler'
 import { chatVdom } from './src/render/vdoms'
 
-/**
- * Main method to initialize render and chat listeners
- */
-const init = (id) => {
-  const wrapperId = id || 'vanilla-js-chat'
-  const chatTarget = document.querySelector(`#${wrapperId}`)
+const VanillaJsChat = () => {
+  /**
+   * Main method to initialize render and chat listeners
+  */
+  const init = (id) => {
+    const wrapperId = id || 'vanilla-js-chat'
+    const chatTarget = document.querySelector(`#${wrapperId}`)
 
-  DomHandler.render(chatTarget, chatVdom)
+    DomHandler.render(chatTarget, chatVdom)
+  }
+
+  const restart = () => {
+    // code
+  }
+
+  const sendMessage = () => {
+    // code
+  }
+
+  const onSend = () => {
+    // code
+  }
+
+  const onClose = () => {
+    // code
+  }
+
+  return {
+    init,
+    restart,
+    sendMessage,
+    onSend,
+    onClose,
+  }
 }
 
-const restart = () => {
-  // code
-}
-
-const sendMessage = () => {
-  // code
-}
-
-const onSend = () => {
-  // code
-}
-
-const onClose = () => {
-  // code
-}
-
-const VanillaJsChat = {
-  init,
-  restart,
-  sendMessage,
-  onSend,
-  onClose,
-}
-
-window.VanillaJsChat = VanillaJsChat
+window.VanillaJsChat = new VanillaJsChat()
