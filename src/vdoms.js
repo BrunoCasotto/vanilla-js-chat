1fb184d4fe007bc6f29b2de0db76771d3ed7c145
Bruno Casotto <casottoalves@hotmail.com>
2019-12-14 10:34:00 -0300

    Basic style created

--- src/vdoms.js
+++ src/vdoms.js
@@ -1,7 +1,7 @@
-export const chatVdom = {
+const headerVdom = {
   type: 'div',
   props: {
-    class: 'vanilla-js-chat'
+    class: 'vanilla-js-chat__header'
   },
   children: [{
     type: 'h1',
@@ -10,4 +10,39 @@ export const chatVdom = {
       textContent: 'Conversa'
     },
   }]
+}
+
+const bodyVdom = {
+  type: 'div',
+  props: {
+    class: 'vanilla-js-chat__body'
+  },
+}
+
+const controllerVdom = {
+  type: 'div',
+  props: {
+    class: 'vanilla-js-chat__controller'
+  },
+  children: [
+    {
+      type: 'button',
+      props: {
+        class: 'vanilla-js-chat__btn',
+        textContent: 'enviar'
+      },
+    }
+  ]
+}
+
+export const chatVdom = {
+  type: 'div',
+  props: {
+    class: 'vanilla-js-chat'
+  },
+  children: [
+    headerVdom,
+    bodyVdom,
+    controllerVdom,
+  ]
 }
--- webpack/partials/entry.js
+++ webpack/partials/entry.js
@@ -1,9 +1,14 @@
 const path = require('path')
+const { isDev } = require('./../utils/enviroment')
 const { rootPath, stylePath } = require('./../utils/paths')
 
 const entry = []
 
 entry.push(path.join(rootPath, 'index.js'))
 entry.push(path.join(stylePath, 'index.scss'))
+
+if(isDev) {
+  entry.push(path.join(stylePath, 'base.scss'))
+}
 
 module.exports = entry
